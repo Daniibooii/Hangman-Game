c524d4b458d663f8f281353562a2bc42c02175a9
unknown <gw-danmace@hotmail.com>
2018-02-08 19:22:43 -0500

    close-ish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,63 @@
-var words = [word1, word2, word3, word4, etc.]
-//these will need to be strings?
 
-//somehow prompt user that the game is beginning and how to play it
+
+
+var words = ["w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9", "w10"];
+
+var guesses = 5;
+//var wordsIndex = 0; do I need this?//
+//var wordsArray = [words.w1, words.w2, words.w3, words.w4, words.w5, words.w6, words.w7, words.w8, words.w9, words.w10]; do I need this?
+
+var userGuess = ;
+
+var correctLetter = ;
+
+var computerChoice = ;
+
 document.onkeyup = function(event) {
-var letter = String.fromCharCode(event.which).toLowerCase();
-var computerChoice = words[Math.floor(Math.random() * words.length)];
+  var userGuess = event.key;
+  var userGuess = userGuess.toLowerCase();
 
+  if (userGuess === correctLetter) {
+    document.write("A lucky guess");
+  }
+  else {
+    var newGuesses = guesses - 1;
 
-
-}
-
-
-
-
-//sample code below
-
-
-
-
+    document.write("Not today");
+  }
 
 
 
 
+};
 
-// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
-      document.onkeyup = function(event) {
 
-        // Captures the key press, converts it to lowercase, and saves it to a variable.
-        var letter = String.fromCharCode(event.which).toLowerCase();
+// code snippets below //
 
-        // If the letter is h, run the following functions/methods.
-        if (letter === "h") {
-          car.honk();
-          reWriteStats();
-        }
+//document.write(words[random]);
 
-        // If the letter is d, run the following functions/methods.
-        if (letter === "d") {
-          car.driveToWork();
-          reWriteStats();
-        }
+//for (var i = 0; i < vegetables.length; i++) {
+//        console.log("I love " + vegetables[i]);
+//      }
 
-        // If the letter is w, run the following functions/methods.
-        if (letter === "w") {
-          car.driveAroundWorld();
-          reWriteStats();
-        }
+//function logArray(list) {
+//        for (var j = 0; j < list.length; j++) {
+  //        console.log(list[j]);
+  //      }
+  //    }
 
-        // If the letter is t, run the following functions/methods.
-        if (letter === "t") {
-          car.getTuneUp();
-          reWriteStats();
-        }
-      };
+//      var userText = document.getElementById("user-text");
+
+  //      document.onkeyup = function(event) {
+  //        userText.textContent = event.key;
+
+//var userGuess = event.key;
+
+//var computerChoice = words[Math.floor(Math.random() * words.length)];
+
+//document.onkeyup = function(event) {
+
+
+//function updateScore() {
+  //document.querySelector("#score").innerHTML = "Score: " + score;
+//}
